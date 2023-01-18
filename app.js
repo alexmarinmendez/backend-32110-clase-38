e4a96a2edf969fd7f15094fcd4224d36b08a0ffc
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-17 20:46:41 -0500

    feat: add product entities

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const router = require('./routes/user.route')
+const { userRouter, productRouter } = require('./routes')
 
 const app = express()
 const PORT = 3000
 app.use(express.json())
 app.listen(PORT, () => console.log('Server up'))
 
-router(app)
+userRouter(app)
+productRouter(app)
